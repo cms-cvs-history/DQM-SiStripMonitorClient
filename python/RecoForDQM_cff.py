6c549017879c556ed652709a7b0672c2296ad48b
cmsbuild <>
2008-06-29 21:44:21 +0000

    This commit was manufactured by cvs2git to create tag 'DQM- SiStripMonitorClient-CMSSW_2_0_10'.
    Sprout from master 2008-06-07 13:26:32 UTC Suchandra Dutta <sha1-66dec20152d9caa90fb44e3138a83d284b0c6de4@cern.ch> 'bug fix in javascript file + correction in SiStripSourceConfig.cff/py files'
    Cherrypick from master 2008-04-21 01:35:34 UTC Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch> 'second run of translating configs':
        data/RecoForDQM.cff
        python/RecoForDQM_cff.py
        python/SiStripSourceConfig_cff.py
        test/SiStripDQM_SimData_Offline.cfg
        test/SiStripDQM_SimData_Online.cfg
    Cherrypick from master 2008-06-29 21:44:20 UTC Suchandra Dutta <sha1-66dec20152d9caa90fb44e3138a83d284b0c6de4@cern.ch> 'updated the reportSummary following standardization planning from DQM group':
        interface/SiStripActionExecutor.h
        interface/SiStripUtility.h
        src/SiStripActionExecutor.cc
        src/SiStripUtility.cc
    Cherrypick from master 2008-04-30 14:51:18 UTC Suchandra Dutta <sha1-66dec20152d9caa90fb44e3138a83d284b0c6de4@cern.ch> 'added hardware monitoring':
        data/SiStripSourceConfig.cff
    Cherrypick from master 2008-06-09 12:12:10 UTC Suchandra Dutta <sha1-66dec20152d9caa90fb44e3138a83d284b0c6de4@cern.ch> 'temporary commit of  SiStripSourceConfigP5.cff for 20X changing parameter for Tracking Monitor':
        data/SiStripSourceConfigP5.cff
    Delete:
        data/RecoForDQM_Sim.cff
        python/RecoForDQM_Sim_cff.py
        python/SiStripDQMTier0GlobalRun_cff.py
        python/SiStripDQMTier0_cff.py
        python/SiStripSourceConfigP5_cff.py
        python/SiStripSourceConfigTier0_cff.py

--- python/RecoForDQM_cff.py
+++ python/RecoForDQM_cff.py
@@ -1,16 +1,21 @@
 import FWCore.ParameterSet.Config as cms
 
-# Digitiser ####
-from EventFilter.SiStripRawToDigi.SiStripDigis_cfi import *
 # Zero Suppression  ####
-from RecoLocalTracker.SiStripZeroSuppression.SiStripZeroSuppression_cfi import *
+from RecoLocalTracker.SiStripZeroSuppression.SiStripZeroSuppression_SimData_cfi import *
+# Digitiserr ####
+from EventFilter.SiStripRawToDigi.SiStripDigis_cfi import *
 # Cluster Finder ####
-from RecoLocalTracker.SiStripClusterizer.SiStripClusterizer_cfi import *
+from RecoLocalTracker.SiStripClusterizer.SiStripClusterizer_RealData_cfi import *
 from RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitMatcher_cfi import *
 # TrackRefitter ####
 from RecoTracker.TrackProducer.RefitterWithMaterial_cff import *
-RecoModulesForTIFData = cms.Sequence(siStripDigis*siStripZeroSuppression*siStripClusters)
-siStripDigis.ProductLabel = 'source'
+# TrackInfo ####
+from AnalysisAlgos.TrackInfoProducer.TrackInfoProducer_cfi import *
+RecoModulesForSimData = cms.Sequence(siStripZeroSuppression*TrackRefitter*trackinfo)
+RecoModulesForTIFData = cms.Sequence(SiStripDigis*siStripZeroSuppression*siStripClusters)
+SiStripDigis.ProductLabel = 'source'
 siStripClusters.SiStripQualityLabel = 'test1'
 TrackRefitter.TrajectoryInEvent = True
+trackinfo.cosmicTracks = 'TrackRefitter'
+trackinfo.rechits = 'TrackRefitter'
 
