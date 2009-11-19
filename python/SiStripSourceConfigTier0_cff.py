853d74727ecce65cfcd0f2e259014bf407219853
cmsbuild <>
2009-11-19 20:58:31 +0000

    This commit was manufactured by cvs2git to create tag 'DQM- SiStripMonitorClient-V03-03-66-02'.
    Sprout from master 2009-10-30 15:54:53 UTC Suchandra Dutta <sha1-66dec20152d9caa90fb44e3138a83d284b0c6de4@cern.ch> 'removing debug printing from SiStripQualityChecker'
    Cherrypick from master 2009-11-19 20:58:30 UTC Suchandra Dutta <sha1-66dec20152d9caa90fb44e3138a83d284b0c6de4@cern.ch> '*** empty log message ***':
        data/sistrip_qualitytest_config_tier0.xml
        python/SiStripSourceConfigP5_cff.py
        python/SiStripSourceConfigTier0_cff.py
        src/SiStripInformationExtractor.cc
        src/SiStripTrackerMapCreator.cc
        src/SiStripWebInterface.cc
    Cherrypick from master 2009-11-19 20:30:28 UTC Suchandra Dutta <sha1-66dec20152d9caa90fb44e3138a83d284b0c6de4@cern.ch> 'updating QTest config files for collision':
        data/sistrip_qualitytest_config.xml

--- python/SiStripSourceConfigTier0_cff.py
+++ python/SiStripSourceConfigTier0_cff.py
@@ -15,6 +15,12 @@ from DQM.SiStripMonitorHardware.siStripFEDMonitor_Tier0_cff import *
 # SiStripMonitorDigi ####
 from DQM.SiStripMonitorDigi.SiStripMonitorDigi_cfi import *
 SiStripMonitorDigi.Mod_On = False
+SiStripMonitorDigi.TProfDigiApvCycle.subdetswitchon = True
+
+# SiStripMonitorCluster ####
+from DQM.SiStripMonitorCluster.SiStripMonitorCluster_cfi import *
+SiStripMonitorCluster.Mod_On = False
+SiStripMonitorCluster.TProfClustersApvCycle.subdetswitchon = True
 
 # SiStripMonitorTrack ####
 from DQM.SiStripMonitorTrack.SiStripMonitorTrack_cfi import *
@@ -30,12 +36,8 @@ MonitorTrackResiduals.Mod_On = False
 
 # TrackingMonitor ####
 from DQM.TrackingMonitor.TrackingMonitor_cfi import *
-TrackMon.TrackProducer = 'generalTracks'
-TrackMon.AlgoName = 'CKFTk'
-TrackMon.FolderName = 'Tracking/TrackParameters'
-TrackMon.doSeedParameterHistos = True
-TrackMon.doBeamSpotPlots       = True
-TrackMon.BSFolderName          = 'Tracking/TrackParameters/BeamSpotParameters'
+TrackMon.FolderName          = 'Tracking/TrackParameters'
+TrackMon.BSFolderName        = 'Tracking/TrackParameters/BeamSpotParameters'
 
 # DQM Services
 dqmInfoSiStrip = cms.EDFilter("DQMEventInfo",
